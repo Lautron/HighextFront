bb3f2f50efbb568cf74f4ebf629cbd3414192892
Lautaro Bachmann <laurigbach@gmail.com>
2025-11-19 16:56:27 -0300

    Add basic logic/state to colorConfig

--- src/components/pages/ColorConfig.tsx
+++ src/components/pages/ColorConfig.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FORMAT_OPTIONS, type ColorFormatConfig } from '@/lib/constants';
 
-const ColorConfig = () => {
+interface ColorConfigProps {
+  colors: string[];
+}
+
+const ColorConfig: React.FC<ColorConfigProps> = ({ colors }) => {
+  const values = Object.values(FORMAT_OPTIONS);
+  const [colorConfig, setColorConfig] = useState<ColorFormatConfig>(() => {
+    const initialState: ColorFormatConfig = {};
+    colors.forEach((color, ind) => {
+      initialState[color] =
+        ind < values.length ? values[ind] : values[values.length - 1];
+    });
+    return initialState;
+  });
+
+  const handleFormatChange = (color: string, newFormat: string): void => {
+    setColorConfig((prevState) => {
+      return {
+        ...prevState,
+        [color]: newFormat,
+      };
+    });
+  };
+
   return (
     <div>
-      {/* Color configuration UI will go here */}
+      <h1>This is the color config!</h1>
+      {colors.map((color) => (
+        <div
+          key={color}
+          style={{ display: 'flex', alignItems: 'center', marginBottom: 8 }}
+        >
+          <span style={{ background: color }}>{color}</span>
+          <select
+            value={colorConfig[color]}
+            onChange={(e) => handleFormatChange(color, e.target.value)}
+          >
+            {Object.entries(FORMAT_OPTIONS).map(([key, val]) => (
+              <option key={key} value={val}>
+                {key}
+              </option>
+            ))}
+          </select>
+        </div>
+      ))}
     </div>
   );
 };
