c21dbf241d0ca2cfc83c21b707cfe8a29f87a11d
Lautaro Bachmann <laurigbach@gmail.com>
2025-11-19 18:21:29 -0300

    Add MarkdownViewer component

---
+++ src/components/pages/MarkdownViewer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+interface MarkdownViewerProps {
+  markdown: string;
+}
+
+const MarkdownViewer: React.FC<MarkdownViewerProps> = ({ markdown }) => {
+  return <ReactMarkdown>{markdown}</ReactMarkdown>;
+};
+
+export default MarkdownViewer;
--- src/components/pages/ResultsDisplay.tsx
+++ src/components/pages/ResultsDisplay.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import MarkdownViewer from './MarkdownViewer';
 
 interface ResultsDisplayProps {
   markdown: string;
 }
 
 const ResultsDisplay: React.FC<ResultsDisplayProps> = ({ markdown }) => {
-  return <div>{markdown}</div>;
+  return <MarkdownViewer markdown={markdown} />;
 };
 
 export default ResultsDisplay;
