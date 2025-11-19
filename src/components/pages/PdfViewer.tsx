de7f481c8ec727056dcc3974f3eac721bf77e123
Lautaro Bachmann <laurigbach@gmail.com>
2025-11-19 18:53:42 -0300

    Add pdfViewer component

--- src/components/pages/PdfViewer.tsx
+++ src/components/pages/PdfViewer.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
-const PdfViewer = () => {
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.mjs',
+  import.meta.url,
+).toString();
+
+interface PdfViewerProps {
+  pdfUrl: string | null;
+}
+
+const PdfViewer: React.FC<PdfViewerProps> = ({ pdfUrl }) => {
+  const [numPages, setNumPages] = useState<number>();
+
+  function onDocumentLoadSuccess({ numPages }: { numPages: number }): void {
+    setNumPages(numPages);
+  }
+
+  if (!pdfUrl) {
+    return <div>No PDF to display.</div>;
+  }
+
   return (
-    <div>
-      {/* Results display UI will go here */}
-    </div>
+    <Document file={pdfUrl} onLoadSuccess={onDocumentLoadSuccess}>
+      {Array.from(new Array(numPages), (el, index) => (
+        <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+      ))}
+    </Document>
   );
 };
 
