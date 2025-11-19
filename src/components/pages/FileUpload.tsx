d137c4806abf94d436c72be4d7cece0357ab1a78
Lautaro Bachmann <laurigbach@gmail.com>
2025-11-19 12:51:43 -0300

    Create basic fileUpload component skeleton

--- src/components/pages/FileUpload.tsx
+++ src/components/pages/FileUpload.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useCallback} from 'react'
+import {useDropzone} from 'react-dropzone'
 
-const FileUpload = () => {
+function FileUpload() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div>
-      {/* File upload UI will go here */}
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the files here ...</p> :
+          <p>Drag 'n' drop some files here, or click to select files</p>
+      }
     </div>
-  );
-};
+  )
+}
 
 export default FileUpload;
