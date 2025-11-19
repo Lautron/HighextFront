d10a4d2a98aec437b26384c9cf83fca0ca344a71
Lautaro Bachmann <laurigbach@gmail.com>
2025-11-19 13:26:46 -0300

    Call analyzePdf when a file is uploaded

--- src/components/pages/FileUpload.tsx
+++ src/components/pages/FileUpload.tsx
@@ -1,22 +1,27 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
+interface FileUploadProps {
+    onFileSelect: (file: File) => void
+}
+function FileUpload({ onFileSelect }: FileUploadProps) {
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        if (acceptedFiles.length > 0) {
+            onFileSelect(acceptedFiles[0])
+        }
+    }, [onFileSelect])
 
-function FileUpload() {
-  const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
-  return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
-  )
+    return (
+        <div {...getRootProps()}>
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <p>Drag 'n' drop some files here, or click to select files</p>
+            }
+        </div>
+    )
 }
 
 export default FileUpload;
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react';
 import FileUpload from "@/components/pages/FileUpload";
 import ColorConfig from "@/components/pages/ColorConfig";
 import ResultsDisplay from "@/components/pages/ResultsDisplay";
+import { analyzePdf } from "@/lib/api";
 
 const Home = () => {
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center my-8">PDF Highlighter Extractor</h1>
-      <FileUpload />
-      <ColorConfig />
-      <ResultsDisplay />
-    </div>
-  );
+    const [pdfFile, setFile] = useState<File>()
+    const [colors, setColors] = useState<string[]>()
+
+    const handleFileSelect = async (file: File): Promise<void> => {
+        setFile(file)
+        const colors = await analyzePdf(file)
+        setColors(colors)
+    };
+    return (
+        <div className="container mx-auto p-4">
+            <h1 className="text-3xl font-bold text-center my-8">PDF Highlighter Extractor</h1>
+            <FileUpload onFileSelect={handleFileSelect} />
+            {colors !== undefined && <ColorConfig />}
+            <ResultsDisplay />
+        </div>
+    );
 };
 
 export default Home;
